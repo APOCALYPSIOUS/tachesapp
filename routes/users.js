f4ca9aa5b753c37fd7f24db2cfaa77501ba09efd
APOCALYPSIOUS <85230304+APOCALYPSIOUS@users.noreply.github.com>
2023-05-22 11:57:57 +0100

    hey

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const User = require("../model/User");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/',async (req,res)=>{
+  try{
+    const users = await User.find();
+    res.json(users);
+  }catch (err) {
+    res.status(500).json({message: "err.message"});
+  }
+}
+  );
+
+router.post('/',async (req,res)=>{
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+  try{
+    const newUser = await user.save();
+    res.json(newUser);
+  }catch (err) {
+    res.status(500).json({message: "err.message"});
+  }
+})
 
 module.exports = router;
