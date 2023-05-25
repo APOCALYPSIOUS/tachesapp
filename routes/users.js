ad5b2148c90c0302573c57a18de1ef7e42804b07
APOCALYPSIOUS <85230304+APOCALYPSIOUS@users.noreply.github.com>
2023-05-25 15:27:00 +0100

    user ready

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,6 @@ router.get('/',async (req,res)=>{
 
 router.post('/',async (req,res)=>{
   const user = new User({
-    id: req.body.id,
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
@@ -27,5 +26,38 @@ router.post('/',async (req,res)=>{
     res.status(500).json({message: "err.message"});
   }
 })
+
+router.delete('/:id',async (req,res)=>{
+  try{
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.json(user);
+  }catch (err) {
+    res.status(500).json({message: "err.message"});
+  }
+})
+
+router.put('/:id',async (req,res)=>{
+  try{
+    const user = await User.findByIdAndUpdate(req.params.id,{
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    res.json(user);
+  }catch (err) {
+    res.status(500).json({message: "err.message"});
+  }
+})
+
+router.get('/:id',async (req,res)=>{
+  try{
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  }catch (err) {
+    res.status(500).json({message: "err.message"});
+  }
+})
+
+
 
 module.exports = router;
