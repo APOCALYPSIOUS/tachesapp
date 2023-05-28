0c1ec49328007439fb4ef12cc090bb59b5269d93
APOCALYPSIOUS <85230304+APOCALYPSIOUS@users.noreply.github.com>
2023-05-28 12:55:38 +0100

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 var express = require('express');
-
+var cors = require('cors')
 
 const db = require('./db');
 var app = express();
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('port', 3000);
